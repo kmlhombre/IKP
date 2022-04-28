b43f4ff9d28d722c52b54cd49b3999697e2e6bd2
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2022-04-28 13:07:09 +0200

    Index website+css

--- IKP_app/general_app/urls.py
+++ IKP_app/general_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    path('main/', views.main, name='index'),
+    path('login_user/', views.login_user, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
