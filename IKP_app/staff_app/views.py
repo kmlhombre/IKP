3c3ce72742842fe3a030c907d20b4aaa8617fbc9
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2022-06-29 19:03:03 +0200

    examinations analyze

--- IKP_app/staff_app/views.py
+++ IKP_app/staff_app/views.py
@@ -7,15 +7,16 @@ def index(request):
     return render(request, 'staff-index.html', {'role': navbar_staff(request)})
 
 def analyze_single_examination(request):
-
-    unaccepted_examination = UnacceptedExaminations.objects.filter().order_by('id')[0]
+    unaccepted_examinations = UnacceptedExaminations.objects.filter().order_by('id')
+    examinations_left = len(unaccepted_examinations)-1
+    unaccepted_examination = unaccepted_examinations[0]
     #patient_pesel = unaccepted_examination.patient_pesel
 
     #next_id = UnacceptedExaminations.objects.order_by('-id').first().id + 1
 
     request_uri = request.build_absolute_uri()
     file_path = request_uri + "/file?hash=" + unaccepted_examination.document_content.name.replace('unaccepted_examinations/', '')
-    return render(request, 'staff-analyze-examinations.html', {'examination': unaccepted_examination, 'file_path' : file_path})
+    return render(request, 'staff-analyze-examinations.html', {'examination': unaccepted_examination, 'file_path' : file_path, 'examinations_left' : examinations_left})
 
 
 def discard_examination(request):
